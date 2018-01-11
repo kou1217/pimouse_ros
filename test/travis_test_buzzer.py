531c88ee5910e6c05321036d20744b29d8cbd30f
Kohei Kyoya <k.kyoya1217@gmail.com>
2018-01-11 19:23:52 +0900

    Add a test script

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -3,12 +3,12 @@ import rospy, unittest, rostest
 import rosnode
 import time
 
-class BuzzerTest(unittest.TestCase)
+class BuzzerTest(unittest.TestCase):
     def test_node_exist(self):
         nodes = rosnode.get_node_names()
         self.assertIn('/buzzer', nodes, "node dos not exit")
 
-if _name_=='_main_":
+if __name__=='__main__':
     time.sleep(3)
     rospy.init_node('travis_test_buzzer')
     rostest.rosrun('pimouse_ros', 'travis_test_buzzer', BuzzerTest)
